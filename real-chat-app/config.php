d0eec1ff067f06c9e947defce2c5048a8cc18d2c
stevegtdbz <59664812+stevegtdbz@users.noreply.github.com>
2022-08-29 21:58:29 +0300

    Update config.php

--- real-chat-app/config.php
+++ real-chat-app/config.php
@@ -1,7 +1,6 @@
 <?php
 
 // Project Settings
-//$projectDomain = "192.168.1.15"; // IP or Domain
 $projectDomain = "127.0.0.1"; // IP or Domain
 
 $projectName = "Real Chat App";
@@ -9,9 +8,9 @@ $projectPath = "http://".$projectDomain."/real-chat-app";
 
 // Mysql Database Settings
 $dbHost = "localhost";
-$dbName = "real_chat_app";
-$dbUsername = "blackhat";
-$dbPassword = "freelancer112";
+$dbName = "";
+$dbUsername = "";
+$dbPassword = "";
 
 // Server Settings
 $chat_server_host = "localhost";
